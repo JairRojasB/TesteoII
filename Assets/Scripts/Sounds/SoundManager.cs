377780c7c5024f43e968bf31076d650893314fd5
SingerSilent <72463967+SingerSilent@users.noreply.github.com>
2024-01-27 13:32:36 -0500

    This

---
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip[] goodClips, badClips;
+
+    public AudioListener audioListener;
+
+    private void PlaySelecctedListener()
+    {
+        
+    }
+}

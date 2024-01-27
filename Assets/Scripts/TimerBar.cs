61d48ab66e4bdc79f0aae589f17a75309a1b33cb
JairRojasB <jairrojasb2505@gmail.com>
2024-01-26 23:56:13 -0500

    BarraXD

---
+++ Assets/Scripts/TimerBar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerBar : MonoBehaviour
+{
+    public Slider slider;
+    [SerializeField] private int _maxTime;
+
+    void Start()
+    {
+        slider.maxValue = _maxTime;
+        slider.value = _maxTime;
+    }
+
+    void Update()
+    {
+        //Resta el tiempo reduciendo la barra
+        slider.value = slider.value - Time.deltaTime;
+        EndTime();
+    }
+
+    public void SetMaxTime(int time) 
+    {
+        slider.maxValue = time;
+        slider.value = time;
+    }
+
+    public void EndTime() 
+    {
+        if (slider.value == 0) 
+        {
+            //Invocar método perder/generar nuevas flechas/lo que tenga que pasar cuando se acabe el tiempo
+            //print("AEA");
+        }
+    }
+}

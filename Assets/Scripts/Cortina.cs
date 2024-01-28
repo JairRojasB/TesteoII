5ab21d0e2d6e97cdf046553513e43e0360f0626f
JairRojasB <jairrojasb2505@gmail.com>
2024-01-28 09:45:47 -0500

    Cortinaa

--- Assets/Scripts/Cortina.cs
+++ Assets/Scripts/Cortina.cs
@@ -40,14 +40,15 @@ public class Cortina : MonoBehaviour
     {
         yield return new WaitForSeconds(0.5f);
 
+        if (_fade != null) { _fade.ScreenFadeToBlack(); }
+
+        yield return new WaitForSeconds(0.5f);
+
+        gameObject.transform.DOScale(new Vector2(1, 1), 0.5f).SetEase(Ease.InFlash);
+
+        yield return new WaitForSeconds(0.7f);
+
         gameObject.GetComponent<Image>().sprite = _cortinaCerrado;
-
-        yield return new WaitForSeconds(0.5f);
-
-        gameObject.transform.DOScale(new Vector2(1, 5), 0.5f).SetEase(Ease.InFlash);
-
-        yield return new WaitForSeconds(0.5f);
-
         if (_fade != null) { _fade.SceenFaceToSolid(); }
     }
 }

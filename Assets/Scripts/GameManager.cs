de87935d37260bdf28f07eed94524b4beb48ab90
SingerSilent <72463967+SingerSilent@users.noreply.github.com>
2024-01-27 23:34:31 -0500

    CamShake

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,9 +10,21 @@ public class GameManager : MonoBehaviour
 
     private void Awake() => StarThisGame(false);
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ShakeCamera();
+        }
+    }
     public void StarThisGame(bool esta)
     {
         timer.gameObject.SetActive(esta);
         arrowSpawn.gameObject.SetActive(esta);
+    }
+
+    public void ShakeCamera()
+    {
+        Camera.main.DOShakeRotation(1,5,5,5).OnComplete(()=>Camera.main.transform.position = new Vector3(0,0,-10));
     }
 }
